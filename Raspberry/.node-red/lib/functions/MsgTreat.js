d30235fcf2aa9a35e3dce6f79841662a47d62fd6
MA Dingjie <mdj2812@gmail.com>
2016-11-03 17:58:42 +0100

    Node V0.0.1
    + La fonction de split est finie

--- Raspberry/.node-red/lib/functions/MsgTreat.js
+++ Raspberry/.node-red/lib/functions/MsgTreat.js
@@ -1,8 +1,48 @@
 // name: MsgTreat
-// outputs: 2
-
+// outputs: 1
 //slpit the msg
-var FT, NF, SID, WID, FSeq, S1, S2, S3;
-var NewMsg, ErrMsg;
+var FT, NB, SID, WID, FSeq, S1, S2, S3, S4;
+var PS, MF, TEMP, BAT;
+var outputMsgs = {};
+var sensors = msg.payload.split("#");
+var i = 0;
 
-return [NewMsg, ErrMsg];
+for (var w in sensors) {
+    switch(i)
+    {
+        case 0:
+            FT = sensors[w];
+            break;
+        case 1:
+            SID = sensors[w];
+            break;
+        case 2:
+            WID = sensors[w];
+            break;
+        case 3:
+            FSeq = sensors[w];
+            break;
+        case 4:
+            S1 = sensors[w];
+            PS = S1.split(":")[1];
+            break;
+        case 5:
+            S2 = sensors[w];
+            MF = S2.split(":")[1];
+            break;
+        case 6:
+            S3 = sensors[w];
+            TEMP = S3.split(":")[1];
+            break;
+        case 7:
+            S4 = sensors[w];
+            BAT = S4.split(":")[1];
+            break;
+        default:
+    }
+    i++;
+}
+
+    outputMsgs.payload = {WID, PS, MF, TEMP, BAT};
+
+return [outputMsgs];
