df89969a57f0ca0e44dc62005a7b8c5943502e5f
MA Dingjie <mdj2812@gmail.com>
2016-12-23 21:10:18 +0100

    Application avec la map et geolocalisation
    fait par Seynabou, mise-en-ligne par MA

--- Android/01-Application finale/EasyPark4/app/src/main/java/a/easypark4/Activity/MainActivity.java
+++ Android/01-Application finale/EasyPark4/app/src/main/java/a/easypark4/Activity/MainActivity.java
@@ -1,17 +1,41 @@
 package a.easypark4.Activity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 
 import java.util.HashMap;
 
@@ -22,7 +46,7 @@ import a.easypark4.helper.SessionManager;
 
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private SQLiteHandler db;
     private SessionManager session;
@@ -32,6 +56,19 @@ public class MainActivity extends AppCompatActivity
     private String name;
     private String email;
     private View navHeaderView;
+
+    // Map variables
+    private static int PERMISSION_GPS = 100;
+    SupportMapFragment sMapFragment;
+    private LocationManager locationManager;
+    private GoogleMap mMap;
+    private LocationRequest mLocationRequest;
+    private Location mLocation;
+    private GoogleMap mGoogleMap;
+    private GoogleApiClient mGoogleApiClient;
+    private Marker marker;
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,7 +118,42 @@ public class MainActivity extends AppCompatActivity
         txtName.setText(name);
         txtEmail.setText(email);
 
+        // Provide access to the system location services
+        locationManager =(LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //Register for location updates using the named provider, and a pending intent
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
 
+
+
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(60 * 1000)        // 60 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        // Check if the given provider is enabled
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            createGpsDisabledAlert();
+        }
+
+        // Check app location permission
+        if(ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_GPS); }
+        else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,MainActivity.this);
+        }
+
+        sMapFragment = SupportMapFragment.newInstance();
+
+        sMapFragment.getMapAsync(this);
     }
 
     @Override
@@ -121,28 +193,109 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentManager sFm = getSupportFragmentManager();
+
+
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.btnMenuMap) {
+            if(!sMapFragment.isAdded())
+                sFm.beginTransaction().add(R.id.map, sMapFragment).commit();
+            else
+                sFm.beginTransaction().show(sMapFragment).commit();
 
-        }
-        else if (id == R.id.btnMenuAccountSetting) {
+        } else if (id == R.id.btnMenuAccountSetting) {
 
-        }
-        else if (id == R.id.btnMenuLogout){
+        } else if (id == R.id.btnMenuLogout) {
             logoutUser();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestcode, @NonNull String [] permissions, @NonNull int[] grantResults){
+        if(requestcode == PERMISSION_GPS){
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,MainActivity.this);
+            }
+            else {
+                Toast.makeText(MainActivity.this,"Permission refusée !",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            mLocation = location;
+            onHandleMap(mLocation, mGoogleMap);
+
+        } else {
+            Toast.makeText(MainActivity.this, "Impossible de générer les coordonnées GPS", Toast.LENGTH_LONG).show();
+        }
+
+    }
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+        Log.i(TAG, "Location services suspended. Please reconnect.");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(TAG, "Location services failed. Please reconnect.");
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location == null) {
+
+            Toast.makeText(MainActivity.this, "En cours de localisation ... ", Toast.LENGTH_LONG).show();
+        }
+
+
     }
 
     /**
      * Logging out the user. Will set isLoggedIn flag to false in shared
      * preferences Clears the user data from sqlite users table
-     * */
+     */
     private void logoutUser() {
         session.setLogin(false);
 
@@ -153,4 +306,64 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);
         finish();
     }
+
+    /*
+     * Creates an alert dialog to use when GPS isn't enabled and offers to activate it
+     */
+    private void createGpsDisabledAlert() {
+        AlertDialog.Builder localBuilder = new AlertDialog.Builder(this);
+        localBuilder
+                .setMessage("Le GPS est inactif, voulez-vous l'activer ?")
+                .setCancelable(false)
+                .setPositiveButton("Activer GPS ",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                                MainActivity.this.showGpsOptions();
+                            }
+                        }
+                );
+        localBuilder.setNegativeButton("Ne pas l'activer ",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        paramDialogInterface.cancel();
+                        MainActivity.this.finish();
+                    }
+                }
+        );
+        localBuilder.create().show();
+    }
+
+    private void showGpsOptions() {
+        startActivity(new Intent("android.settings.LOCATION_SOURCE_SETTINGS"));
+
+
+    }
+
+    public void onHandleMap(Location location, GoogleMap googleMap) {
+        mMap = googleMap;
+        marker = new Marker(googleMap, location);
+        marker.setLocation(location);
+        marker.afficherMarker();
+    }
+
+
+    LocationListener locListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location loc) {
+            double latitude = loc.getLatitude();
+            double longitude = loc.getLongitude();
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+
+        @Override
+        public void onProviderEnabled(String provider) {}
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+
+    };
+
 }
